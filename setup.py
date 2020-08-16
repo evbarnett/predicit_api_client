ecff21672578fa9171c2deceb7600b5c13a48395
evbarnett <evbarnett@protonmail.com>
2020-08-16 16:46:10 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='predictit_client',
+    name='predictit-client',
     version='0.1',
     author="github.com/evbarnett",
     author_email="evbarnett@protonmail.com",
     description="Client API wrapper for PredictIt",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/evbarnett/predicit_api_client",
+    url="https://github.com/evbarnett/predicit-client",
     license='MIT',
     packages=setuptools.find_packages(),
     install_requires=[
